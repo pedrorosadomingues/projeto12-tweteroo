64c4f85997a8662f374249dd04bb040ca0f21bbc
pedrorosadomingues@gmail.com <pedrorosadomingues@gmail.com>
2023-01-07 02:34:35 -0300

    Requisio POST: salvando tweets  num array de tweets do servidor

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ server.use(express.json());
 server.use(cors());
 
 const users = [];
+const tweets = [];
 
 server.post('/sign-up', (req, res) => {
     const user = req.body;
@@ -18,6 +19,27 @@ server.post('/sign-up', (req, res) => {
     users.push(user);
     console.log(users);
     res.send('OK');
+});
+
+server.post('/tweets', (req, res) => {
+    if (users.find(user => user.username === req.body.username) === undefined) {
+        res.status(401).send('UNAUTHORIZED');
+        return;
+    }
+    const tweet = req.body;
+    console.log(req.body);
+    const id = tweets.length + 1;
+
+    tweet.id = id;
+
+    tweets.push(tweet);
+    console.log(tweets);
+    res.send('OK');
+});
+
+server.get('/tweets', (req, res) => {
+    
+    res.send(tweets.splice(0, 10));
 });
 
 const PORT = 5000;
